f6802597cc39ab44513590db2e18baf31ecc3d80
Ansu-dev <ansu1007@naver.com>
2023-09-17 15:30:40 +0900

    feat: service, repository 의존성 주입

--- src/main/java/com/ansu/board/repository/MemoryMemberRepository.java
+++ src/main/java/com/ansu/board/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.ansu.board.repository;
 
 import com.ansu.board.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
     private static final Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
--- src/main/java/com/ansu/board/service/MemberService.java
+++ src/main/java/com/ansu/board/service/MemberService.java
@@ -3,13 +3,21 @@ package com.ansu.board.service;
 import com.ansu.board.domain.Member;
 import com.ansu.board.repository.MemberRepository;
 import com.ansu.board.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     
     public Long join(Member member) {
         // 중복 이름의 회원은 안된다.
@@ -23,13 +31,15 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
+    /** 회원 단일 검색 */
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 
+    /** 중복 이름 검사 메소드 */
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName()).ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            throw new IllegalStateException("이미 존재 하는 회원 입니다.");
         });
     }
 }
